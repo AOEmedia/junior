0d7ece81d4ea7a5f4b797b7646e851379c944ccd
Daniel Zohm <daniel.zohm@aoemedia.de>
2013-10-25 18:11:34 +0200

    MAGEINT-302: reverted the changes

--- src/Junior/Client.php
+++ src/Junior/Client.php
@@ -175,9 +175,20 @@ class Client
             $header .= $this->authHeader;
         }
 
+        // prepare data to be sent
+        $opts    = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => $header,
+                'content' => $json,
+                'timeout' => $this->timeOut,
+            )
+        );
+        $context = stream_context_create($opts);
+
         // try to physically send data to destination
         try {
-            $response = $this->fileGetContents($json);
+            $response = file_get_contents($this->uri, false, $context);
         } catch (\Exception $e) {
             $message = "Unable to connect to {$this->uri}";
             $message .= PHP_EOL . $e->getMessage();
@@ -199,50 +210,6 @@ class Client
 
         // handle response, create response object and return it
         return $this->handleResponse($json_response);
-    }
-
-    /**
-     * @param $json
-     * @return string
-     */
-    public function fileGetContents($json)
-    {
-        $response = '';
-        $url      = str_replace('http://', '', $this->uri);
-        $temp     = explode('/', $url);
-        $domain   = $temp[0];
-        $get      = str_replace($domain, '', $url);
-        $fp       = fsockopen($domain, 80, $errno, $errstr, (int) $this->timeOut);
-
-        if ($fp) {
-            // set timeOut for request
-            stream_set_timeout($fp, (int) $this->timeOut);
-
-            // write data
-            fwrite($fp, "POST $get HTTP/1.0\r\n");
-            fwrite($fp, "Host: $domain\r\n");
-            fwrite($fp, "Content-Length: ".strlen($json)."\r\n");
-            fwrite($fp, "Connection: close\r\n");
-            fwrite($fp, "\r\n");
-            fwrite($fp, $json);
-
-            $info = stream_get_meta_data($fp);
-            while (!feof($fp) && !$info['timed_out']) {
-                $response .= fgets($fp, 4096);
-                $info      = stream_get_meta_data($fp);
-            }
-
-            if ($info['timed_out']) {
-                \Mage::log("Connection Timed Out!");
-                return false;
-            }
-            else {
-                $parsedResponse = explode("\n", $response);
-            }
-            fclose($fp);
-        }
-
-        return trim(end($parsedResponse));
     }
 
     /**
